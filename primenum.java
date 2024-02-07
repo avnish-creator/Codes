74e22592b0bc41a80a13d2b77c820324f17fd817
Avnish Ojha <avnishavnish7@gmail.com>
2024-02-07 17:35:54 +0530

    Revised the prime number code concept.

--- primenum.java
+++ primenum.java
@@ -5,11 +5,21 @@ public class primenum{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int inputnum = sc.nextInt();
-
-        boolean = true;
-        int a = 0;
-        for (int i = 2; i <= inputnum; i++) {
+        
+        boolean x = true;
+        for (int i = 2; i <= inputnum-1; i++) {
+            if(inputnum%2==0){
+                x = false;
+            }
             
         }
+        if (x==true) {
+            System.out.println("It is a prime number.");
+            
+        } 
+            else {
+                System.out.println("It is not a prime number.");
+            
+        }sc.close();
     }
 }
