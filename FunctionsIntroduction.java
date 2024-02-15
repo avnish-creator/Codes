979b20911a64f96572497ab011de1ceb1aee80e8
Avnish Ojha <avnishavnish7@gmail.com>
2024-02-15 12:10:25 +0530

    functions introduction has been done

---
+++ FunctionsIntroduction.java
@@ -0,0 +1,11 @@
+public class FunctionsIntroduction {
+    public static void Helloworld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String[] args) {
+        Helloworld();
+    }
+}
